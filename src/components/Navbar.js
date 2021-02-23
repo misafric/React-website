132a06f4e6d7fa926ebe57ce44ab99ebe7a66f69
misafric <fricovam4@gmail.com>
2021-02-23 12:51:59 +0100

    cards, hero section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { Button } from './Button';
@@ -18,14 +18,19 @@ function Navbar() {
         }
     };
 
+    
+    useEffect (() => {
+        showButton();
+    }, []);
+
     window.addEventListener('resize', showButton);
 
     return (
         <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
-                        travel. <i className="fab fa-typo3"></i>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                        travel. <i className='fab fa-typo3'></i>
                     </Link>
                     <div className='menu-icon' onClick={handleClick}> 
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
